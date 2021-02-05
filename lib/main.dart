ffd0bb9536f6b7e1bb69914f399924565b2b53a5
Rimma <rimma02011998@gmail.com>
2021-02-05 14:09:37 +0200

    animated container 2 variant

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:new_widgets/Cont2.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,27 +11,39 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
-  bool selected = false;
+  var _width = 150.0;
+  var _height = 75.0;
+  var _color = Colors.yellow;
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: GestureDetector(
-          onTap: () {
-            setState(() {
-              selected = !selected;
-            });
-          },
-          child: Center(
-            child: AnimatedContainer(
-              duration: Duration(seconds: 3),
-              curve: Curves.easeIn,
-              width: selected ? 100 : 200,
-              height: selected ? 200 : 100,
-              color: selected ? Colors.red : Colors.green,
-            ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedContainer(
+                duration: Duration(seconds: 2),
+                width: _width,
+                height: _height,
+                color: _color,
+              ),
+              FlatButton(
+                child: Text('Animated'),
+                color: Colors.red,
+                onPressed: () {
+                  setState(() {
+                    _width = 80.0;
+                    _height = 100.0;
+                    _color = Colors.purple;
+
+                  });
+                },
+              ),
+            ],
           ),
         ),
         ),
