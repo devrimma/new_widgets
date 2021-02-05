6ccc4cbbcd01ce5219192afd1f1b1ce5ef1441bf
Rimma <rimma02011998@gmail.com>
2021-02-05 14:20:39 +0200

    clean branch

--- lib/main.dart
+++ lib/main.dart
@@ -11,41 +11,11 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
-  var _width = 150.0;
-  var _height = 75.0;
-  var _color = Colors.yellow;
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              AnimatedContainer(
-                duration: Duration(seconds: 2),
-                width: _width,
-                height: _height,
-                color: _color,
-              ),
-              FlatButton(
-                child: Text('Animated'),
-                color: Colors.red,
-                onPressed: () {
-                  setState(() {
-                    _width = 80.0;
-                    _height = 100.0;
-                    _color = Colors.purple;
-
-                  });
-                },
-              ),
-            ],
-          ),
-        ),
         ),
       );
   }
