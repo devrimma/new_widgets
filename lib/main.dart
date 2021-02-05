deb132e4c0f68792c606d3ba3745a4b9af42d750
Rimma <rimma02011998@gmail.com>
2021-02-05 14:28:58 +0200

    unvisible widgets

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,43 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                  Icons.ac_unit_outlined,
+              size: 50.0,
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Container(
+                    width: 60.0,
+                    height: 30.0,
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+              Opacity(
+                opacity: 0.1,
+                child: Icon(
+                  Icons.share,
+                  size: 70.0,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Container(
+                  width: 30.0,
+                  height: 40.0,
+                  color: Colors.blue,
+                ),
+              ),
+            ],
+          ),
+        ),
         ),
       );
   }
