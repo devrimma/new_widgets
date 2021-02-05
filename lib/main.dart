3bfe79a2677ab86e9d81591d892461deabe5fdd3
Rimma <rimma02011998@gmail.com>
2021-02-05 13:55:31 +0200

    animated container

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:new_widgets/Cont2.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  bool selected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: GestureDetector(
+          onTap: () {
+            setState(() {
+              selected = !selected;
+            });
+          },
+          child: Center(
+            child: AnimatedContainer(
+              duration: Duration(seconds: 3),
+              curve: Curves.easeIn,
+              width: selected ? 100 : 200,
+              height: selected ? 200 : 100,
+              color: selected ? Colors.red : Colors.green,
+              onEnd: () {
+                AnCont();
+              },
+            ),
+          ),
+        ),
+        ),
+      );
+  }
+}
